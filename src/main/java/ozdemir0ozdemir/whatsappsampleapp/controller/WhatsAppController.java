6b0656ac2b1c8cc034fe286ff28538ad7d50501a
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2025-03-15 14:13:01 +0300

    initial commit

--- src/main/java/ozdemir0ozdemir/whatsappsampleapp/controller/WhatsAppController.java
+++ src/main/java/ozdemir0ozdemir/whatsappsampleapp/controller/WhatsAppController.java
@@ -1,2 +1,19 @@
-package ozdemir0ozdemir.whatsappsampleapp.controller;public class WhatsAppController {
+package ozdemir0ozdemir.whatsappsampleapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+class WhatsAppController {
+
+//    @PostMapping("/whatsapp")
+//    void whatsapp(@RequestBody String body) {
+//        System.out.println(body);
+//    }
+
+    @PostMapping("/{path}")
+    void whatsapp(@RequestBody String body, @PathVariable String path) {
+        System.out.println("PATH: " + path);
+        System.out.println("BODY: " + body);
+    }
 }
